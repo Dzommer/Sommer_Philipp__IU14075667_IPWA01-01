1c47e77bac102acab25de7fe1038dbc61408e129
Dzommer <philipp.sommer1@iu-study.org>
2024-07-30 17:22:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const abholungOption = document.getElementById('abholung');
+    const uebergabeOption = document.getElementById('uebergabe');
+    const abholDetails = document.getElementById('abholDetails');
+
+    abholungOption.addEventListener('change', function () {
+        if (abholungOption.checked) {
+            abholDetails.style.display = 'block';
+        }
+    });
+
+    uebergabeOption.addEventListener('change', function () {
+        if (uebergabeOption.checked) {
+            abholDetails.style.display = 'none';
+        }
+    });
+});
 
